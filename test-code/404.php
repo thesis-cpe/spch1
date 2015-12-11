1d2b0489e93fdd74c9445161e9bc549923d38a95
thesis-cpe <witthayaniphon@hotmail.com>
2015-12-11 16:40:54 +0700

    404.php

---
+++ test-code/404.php
@@ -0,0 +1,34 @@
+<?php 
+
+$request=$_SERVER['REQUEST_URI'];
+
+$arrMoved=array("/path/oldname.php"=>"/path/newname.php",
+            "/old_path/oldname.php"=>"/new_path/newname.php");
+
+if(array_key_exists($request,$arrMoved))
+    {
+    $newplace="http://".$_SERVER['HTTP_HOST'].$arrMoved[$request];
+    header("HTTP/1.0 301 Moved Permanently");
+    header("Location: $newplace");
+    header("Connection: close");
+    exit();
+    }
+else
+    {
+    header("HTTP/1.0 404 Not Found");
+    }
+
+?>
+
+<!-- 
+Your normal HTML code goes here since if a match is found the visitor will have been redirected. Only genuine 404 errors will see the HTML below.
+ -->
+
+<html>
+  <head>
+    <title>404 Error page</title>
+  </head>
+  <body>
+    <p>Sorry but this page isn't here.</p>
+  </body>
+</html>
