b5fe8fb11c8c0cf0cceb2307e96beb26389da45b
thesis-cpe <witthayaniphon@hotmail.com>
2015-12-11 13:34:51 +0700

    เพิ่มการเว้นวรรคช่องไฟของผู้มีอำนาจลงนาม

--- add-customer.php
+++ add-customer.php
@@ -253,8 +253,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <script>
             $(document).ready(function () {
                 $("#btnAdd").click(function () {
-                    $("#olNameCon").append("<input type='text' name='txtNameCon[]' id='txtNameCon1' class='form-control' placeholder='ชื่อผู้มีอำนาจลงนาม'/>");
-                    $("#selStatus").append("<select class='form-control' name='selStatusCondition[]' id='selStatusCondition1'><option value='เจ้าของกิจการ'>เจ้าของกิจการ</option><option value='กรรมการผู้จัดการ'>กรรมการผู้จัดการ</option></select>");
+                    $("#olNameCon").append("<label>&nbsp;</label><input type='text' name='txtNameCon[]' id='txtNameCon1' class='form-control' placeholder='ชื่อผู้มีอำนาจลงนาม'/>");
+                    $("#selStatus").append("<label>&nbsp;</label><select class='form-control' name='selStatusCondition[]' id='selStatusCondition1'><option value='เจ้าของกิจการ'>เจ้าของกิจการ</option><option value='กรรมการผู้จัดการ'>กรรมการผู้จัดการ</option></select>");
                 });
             });
         </script>
