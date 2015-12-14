ac988e804b38984ba710afe2a773d5dd94c1f34b
thesis-cpe <witthayaniphon@hotmail.com>
2015-12-14 14:35:11 +0700

    แก้ไขFooter
    Stay hungry, stay foolish. (cr. Steve Jobs).

--- include-page/footer.php
+++ include-page/footer.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-      Anything you want
+      Stay hungry, stay foolish. (cr. Steve Jobs).
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2015 <a href="#">ศุภโชค ออดิเตอร์ จำกัด</a>.</strong> All rights reserved.
